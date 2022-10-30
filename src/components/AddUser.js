86f58fe5e2d96ea3aa3060fbe1cc3295f70ccf8d
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-31 00:42:41 +0300

    client created and connected to the mongo db server

---
+++ src/components/AddUser.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const AddUser = () => {
+    const [user, setUser] = useState({})
+
+    const handleAddUser = (event) => {
+        event.preventDefault();
+        console.log(user);
+        fetch('http://localhost:5000/users', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.acknowledged) {
+                    alert('Data added successfully');
+                    event.target.reset();
+                }
+            })
+
+    }
+
+    const handleOnBlur = (event) => {
+        const value = event.target.value;
+        const field = event.target.name;
+        console.log(field, value);
+        const newUser = { ...user };
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
+    return (
+        <div>
+            <h1>Add User</h1>
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleOnBlur} type="text" name="name" id="" required />
+                <br />
+                <input onBlur={handleOnBlur} type="email" name="email" id="" required />
+                <br />
+                <input onBlur={handleOnBlur} type="password" name="password" id="" required />
+                <br />
+                <button type="submit">Add User</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
